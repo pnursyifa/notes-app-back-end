03bdc69eaa5740ae5c804b5cc7263b16d760bb92
pnursyifa <pnursyifa@gmail.com>
2025-10-27 10:51:05 +0700

    feat auth

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv');
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
 
 const notes = require('./api/notes');
 const NotesService = require('./services/postgres/NotesService');
@@ -9,6 +10,11 @@ const users = require('./api/users/');
 const UsersService = require('./services/postgres/UsersService');
 const UsersValidator = require('./validator/users');
 
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./services/postgres/AuthenticationsService');
+const AuthenticationsValidator = require('./validator/authentications');
+const TokenManager = require('./tokenize/TokenManager');
+
 const ClientError = require('./exceptions/ClientError');
 
 const envFile = process.env.NODE_ENV === 'production' ? '.prod.env' : '.env';
@@ -17,6 +23,7 @@ dotenv.config({ path: envFile });
 const init = async () => {
   const notesService = new NotesService();
   const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -26,6 +33,28 @@ const init = async () => {
         origin: ['*'],
       },
     },
+  });
+
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
+
+  server.auth.strategy('notesapp_jwt', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
   });
 
   await server.register([
@@ -42,6 +71,15 @@ const init = async () => {
         service: usersService,
         validator: UsersValidator,
       },
+    },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
+      }
     },
   ]);
 
