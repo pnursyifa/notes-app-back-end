d4aeed446e6ed057db615531b3f5995a83d3b678
pnursyifa <pnursyifa@gmail.com>
2025-11-02 10:45:14 +0700

    export notes implemented

--- src/server.js
+++ src/server.js
@@ -19,6 +19,10 @@ const collaborations = require('./api/collaborations');
 const CollaborationsService = require('./services/postgres/CollaborationsService');
 const CollaborationsValidator = require('./validator/collaborations');
 
+const _exports = require('./api/exports');
+const ProducerService = require('./services/rabbitmq/ProducerService');
+const ExportsValidator = require('./validator/exports');
+
 const ClientError = require('./exceptions/ClientError');
 
 const envFile = process.env.NODE_ENV === 'production' ? '.prod.env' : '.env';
@@ -92,6 +96,13 @@ const init = async () => {
         collaborationsService,
         notesService,
         validator: CollaborationsValidator,
+      },
+    },
+    {
+      plugin: _exports,
+      options: {
+        service: ProducerService,
+        validator: ExportsValidator,
       },
     },
   ]);
